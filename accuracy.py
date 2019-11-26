712a9e3d91b0cd5879445eca3b3f6aed16328f4f
Aralas <14300180119@fudan.edu.cn>
2019-11-26 10:44:43 +0800

    delete asy results

--- accuracy.py
+++ accuracy.py
@@ -8,13 +8,28 @@ Created on 2019/10/7 8:41
 import numpy as np
 import os
 
-for noise in [3, 5, 7, 9]:
-    acc = np.zeros(5)
+for noise in [3, 5, 7, 8, 9]:
+    acc = []
     for index in range(5):
-        path = os.path.join(os.getcwd(), 'second_sn%d%d_resnet20'%(noise, index))
-        file = open(path + '/test_accuracy.txt')
-        lines = file.readlines()
-        acc[index] = eval(lines[0])
-    print(np.round(np.mean(acc), 3))
+        try:
+            path = os.path.join(os.getcwd(), 'second_sn%d%d_resnet20'%(noise, index))
+            file = open(path + '/test_accuracy.txt')
+            lines = file.readlines()
+            acc.append(eval(lines[0]))
+        except:
+            pass
+    print(noise, np.round(np.mean(acc), 3))
 
+for noise in [2, 3, 4, 5, 6]:
+    acc = []
+    for index in range(5):
+        try:
+            path = os.path.join(os.getcwd(), 'second_an%d%d_resnet20'%(noise, index))
+            file = open(path + '/test_accuracy.txt')
+            lines = file.readlines()
+            acc.append(eval(lines[0]))
+        except:
+            pass
+    if len(acc)>0:
+        print(noise, np.round(np.mean(acc), 3))
 
